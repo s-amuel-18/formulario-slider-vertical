2fd22dbbbe9d78f7bad8cbdfc96d8e48e6e44a7c
Fluxel-Code <110497050+Fluxel-Code@users.noreply.github.com>
2023-08-01 15:23:49 -0400

    se integro el ranger
    se integro el ranger

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ $("[data-mask]").inputmask();
 // * Configuración swiper js
 const swiperSlide = new Swiper(".mySwiper", {
   direction: "vertical",
-  // initialSlide: 0,
+  initialSlide: 2,
   speed: 800,
   allowTouchMove: false,
 });
@@ -94,4 +94,45 @@ function prevSlide() {
 
     nextSlide();
   });
+})();
+
+// !Ranger
+(() => {
+  // Get DOM elements
+  const rangeThumb = document.getElementById("range-thumb");
+  const rangeNumber = document.getElementById("range-number");
+  const rangeLine = document.getElementById("range-line");
+  const rangeInput = document.getElementById("slider");
+
+  // Update the value displayed next to the range input
+  const updateRangeValue = () => {
+    rangeNumber.textContent = rangeInput.value;
+  };
+
+  // Update the position of the range thumb and the width of the range line
+  const updateRangeVisuals = () => {
+    const thumbPosition = rangeInput.value / rangeInput.max;
+    const space = rangeInput.offsetWidth - rangeThumb.offsetWidth;
+
+    // Calculate and set the left position of the range thumb
+    rangeThumb.style.left = thumbPosition * space + "px";
+
+    // Set the width of the range line based on the input value
+    rangeLine.style.width = rangeInput.value + "%";
+  };
+
+  // Add event listener to the range input for real-time updates
+  rangeInput.addEventListener("input", () => {
+    updateRangeValue();
+    updateRangeVisuals();
+  });
+
+  // Initialize the range slider
+  const initializeRangeSlider = () => {
+    updateRangeValue();
+    updateRangeVisuals();
+  };
+
+  // Call the initialization function
+  initializeRangeSlider();
 })();
